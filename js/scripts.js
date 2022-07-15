6a7444484a338f40ceaf1c85eda59f918d42d062
netti-w <106514885+netti-w@users.noreply.github.com>
2022-07-15 18:20:15 +0200

    Change comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 // function with jQuery
 function newItem(){
 
-  // 1. Adding a new item to the list:
+  // Adding a new item to the list:
   let list = $('#list');
   let li = $('<li></li>');
 
@@ -16,24 +16,24 @@ function newItem(){
     list.append(li)
   }
 
-  // 2. Crossing out an item from the list of items:
+  // Crossing out an item from the list of items:
   // // double click event handler adding strike class to li element when clicking list item
   li.on('dblclick', function() {
     li.toggleClass('strike');
   });
 
-  // 3. 3(i). Adding the delete button "X":
+  // Adding the delete button "X":
   let crossOutButton = $('<crossOutButton></crossOutButton>');
 
   crossOutButton.append(document.createTextNode('X'));
   li.append(crossOutButton);
 
-  // 3(ii). Adding CLASS DELETE (DISPLAY: NONE) from the css:
+  // Adding CLASS DELETE (DISPLAY: NONE) from the css:
   // // click event handler adding delete class to li element when clicking 'X'
   crossOutButton.on('click', function() {
     li.addClass('delete');
   });
 
-  // 4. Reordering the items:
+  // Reordering the items:
   list.sortable();
 }
